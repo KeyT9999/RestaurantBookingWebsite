e25be4b84c8d972bbb29a33fc3acba095ba20692
Thang <trankimthang0207@gmail.com>
2025-10-20 16:24:08 +0700

    Done Login

--- src/main/java/com/example/booking/domain/User.java
+++ src/main/java/com/example/booking/domain/User.java
@@ -45,15 +45,11 @@ public class User implements UserDetails {
     @Column(unique = true, nullable = false)
     @Email(message = "Email không hợp lệ")
     @NotBlank(message = "Email không được để trống")
-    @Pattern(regexp = ".*@(gmail\\.com|outlook\\.com\\.vn|yahoo\\.com|hotmail\\.com|student\\.ctu\\.edu\\.vn|ctu\\.edu\\.vn)$", 
-             message = "Email phải thuộc một trong các domain: @gmail.com, @outlook.com.vn, @yahoo.com, @hotmail.com, @student.ctu.edu.vn, @ctu.edu.vn")
     private String email;
     
     @Column(nullable = false)
     @NotBlank(message = "Password không được để trống")
     @Size(min = 8, message = "Password phải ít nhất 8 ký tự")
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", 
-             message = "Mật khẩu phải có ít nhất 8 ký tự, bao gồm chữ hoa, chữ thường, số và ký tự đặc biệt")
     private String password;
     
     @Column(name = "full_name")
